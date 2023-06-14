1dcaa12cbb881543b019c0c61e7153fb6997cf39
Giyas uddin ahmed <108571266+GIYASAHMED@users.noreply.github.com>
2023-06-14 10:42:32 +0000

    ES6DATATYPES

---
+++ basics/ES6.js
@@ -0,0 +1,17 @@
+"use strict";  //treat all js code as newer version
+console.log("welcome to nodes world")
+
+//js premetive detatypes
+
+/*
+nn bb ss u
+n for number and null
+b for boolean and bigInt
+s for string and symbol
+u for undifined
+
+symbol is more use in react
+
+*/
+
+console.log(typeof null);
