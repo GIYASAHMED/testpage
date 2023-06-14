0b0e772f40fd62e6321c85be5df7f3539a822800
Giyas uddin ahmed <108571266+GIYASAHMED@users.noreply.github.com>
2023-06-14 09:57:35 +0000

    newcode

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,9 @@
-console.log("giyas");
+let num = 98
+let userInput 
+
+while(num != userInput){
+    userInput = prompt("enter correct number")
+}
+console.log("correct number")
+
+console.log("giyas ahmed");
